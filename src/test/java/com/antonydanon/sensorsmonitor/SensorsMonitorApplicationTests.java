79f242d36e414b65993d90c2c7d30f6cd7cca496
antonydanon <antondanik32@mail.ru>
2023-10-26 21:14:58 +0300

    Create project.

---
+++ src/test/java/com/antonydanon/sensorsmonitor/SensorsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonydanon.sensorsmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
