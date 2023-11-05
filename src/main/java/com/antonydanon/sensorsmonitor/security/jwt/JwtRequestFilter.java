bcd281d54fd68e167044c6d5b0cf0b5dc7abb543
antonydanon <antondanik32@mail.ru>
2023-11-05 16:02:06 +0300

    Return 401 if token expired.

--- src/main/java/com/antonydanon/sensorsmonitor/security/jwt/JwtRequestFilter.java
+++ src/main/java/com/antonydanon/sensorsmonitor/security/jwt/JwtRequestFilter.java
@@ -2,20 +2,20 @@ package com.antonydanon.sensorsmonitor.security.jwt;
 
 import com.antonydanon.sensorsmonitor.user.model.User;
 import com.antonydanon.sensorsmonitor.user.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 
 @Component
 @RequiredArgsConstructor
@@ -37,7 +37,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             return;
         }
         String jwt = authHeader.substring(7);
-        String email = jwtService.extractEmail(jwt);
+        String email;
+        try {
+            email = jwtService.extractEmail(jwt);
+        } catch (ExpiredJwtException e) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Token is expired.");
+            return;
+        }
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             User user = (User) userService.loadUserByUsername(email);
             if (user.getToken() != null && jwtService.isTokenValid(jwt, user)) {
